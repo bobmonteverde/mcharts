42fd5d95229c7afcab706a4cee5c5d1cacc649a5
Bob Monteverde <bobmonteverde@gmail.com>
2015-08-23 00:35:11 -0400

    Fix tooltop

--- src/tooltip.js
+++ src/tooltip.js
@@ -8,76 +8,6 @@
 (function(window, document, mc) {
   'use strict';
 
-  function calcPos() {
-    switch (gravity) {
-      case 'e':
-        left   = pos[0] - width - dist;
-        top    = pos[1] - (height / 2);
-        tLeft  = left + offsetLeft(container);
-        tTop   = top + offsetTop(container);
-        if (tLeft < scrollLeft) // check if left edge is off screen
-          left = pos[0] > scrollLeft - dist ?
-                   pos[0] + dist :
-                   scrollLeft - tLeft + left;
-        if (tTop < scrollTop) // check if top edge is off screen
-          top  = scrollTop - tTop + top;
-        if (height > scrollTop + windowSize.height - tTop) // check if bottom edge is off screen
-          top  = scrollTop + windowSize.height - tTop + top - height;
-        break;
-
-      case 'w':
-        left   = pos[0] + dist;
-        top    = pos[1] - (height / 2);
-        tLeft  = left + offsetLeft(container);
-        tTop   = top + offsetTop(container);
-        if (width > windowSize.width + scrollLeft - tLeft) // check if right edge is off screen
-          left = pos[0] - width - dist; // TODO: flip gravity, consider checking if left is larger than right
-        if (tTop < scrollTop) // check if top edge is off screen
-          top  = scrollTop + 5;
-        if (height > scrollTop + windowSize.height - tTop) // check if bottom edge is off screen
-          top  = scrollTop - height - 5; // TODO: flip gravity
-        break;
-
-      case 'n':
-        left   = pos[0] - (width / 2);
-        top    = pos[1] + dist;
-        tLeft  = left + offsetLeft(container);
-        tTop   = top + offsetTop(container);
-        if (tLeft < scrollLeft) // check if left edge is off screen
-          left = scrollLeft - tLeft + left;
-        if (width > windowSize.width - tLeft) // check if right edge is off screen
-          left = left + windowSize.width - tLeft - width;
-        if (height > scrollTop + windowSize.height - tTop) { // check is bottom edge goes off screen
-          if (scrollTop - tTop < scrollTop + windowSize.height - tTop - height) { // check if bottom edge is off screen
-            gravity = 's';
-            calcPos();
-          } else {
-            top  = scrollTop + windowSize.height - tTop + top - height;
-          }
-        }
-        break;
-
-      default: // case 's':
-        left   = pos[0] - (width / 2);
-        top    = pos[1] - height - dist;
-        tLeft  = left + offsetLeft(container);
-        tTop   = top + offsetTop(container);
-        if (tLeft < scrollLeft) // check if left edge is off screen
-          left = scrollLeft - tLeft + left;
-        if (width > windowSize.width - tLeft) // check if right edge is off screen
-          left = left + windowSize.width - tLeft - width;
-        if (scrollTop > tTop) { // check if top edge is off screen
-          if (scrollTop - tTop < scrollTop + windowSize.height - tTop - height) {
-            gravity = 'n';
-            calcPos();
-          } else {
-            top  = scrollTop;
-          }
-        }
-        break;
-    }
-  }
-
 
   let mctooltip   = {};
   let removeDelay = 500; // ms to wait before removing a tooltip
@@ -108,6 +38,76 @@
     let top;
     let tLeft;
     let tTop;
+
+    function calcPos() {
+      switch (gravity) {
+        case 'e':
+          left   = pos[0] - width - dist;
+          top    = pos[1] - (height / 2);
+          tLeft  = left + offsetLeft(container);
+          tTop   = top + offsetTop(container);
+          if (tLeft < scrollLeft) // check if left edge is off screen
+            left = pos[0] > scrollLeft - dist ?
+                     pos[0] + dist :
+                     scrollLeft - tLeft + left;
+          if (tTop < scrollTop) // check if top edge is off screen
+            top  = scrollTop - tTop + top;
+          if (height > scrollTop + windowSize.height - tTop) // check if bottom edge is off screen
+            top  = scrollTop + windowSize.height - tTop + top - height;
+          break;
+
+        case 'w':
+          left   = pos[0] + dist;
+          top    = pos[1] - (height / 2);
+          tLeft  = left + offsetLeft(container);
+          tTop   = top + offsetTop(container);
+          if (width > windowSize.width + scrollLeft - tLeft) // check if right edge is off screen
+            left = pos[0] - width - dist; // TODO: flip gravity, consider checking if left is larger than right
+          if (tTop < scrollTop) // check if top edge is off screen
+            top  = scrollTop + 5;
+          if (height > scrollTop + windowSize.height - tTop) // check if bottom edge is off screen
+            top  = scrollTop - height - 5; // TODO: flip gravity
+          break;
+
+        case 'n':
+          left   = pos[0] - (width / 2);
+          top    = pos[1] + dist;
+          tLeft  = left + offsetLeft(container);
+          tTop   = top + offsetTop(container);
+          if (tLeft < scrollLeft) // check if left edge is off screen
+            left = scrollLeft - tLeft + left;
+          if (width > windowSize.width - tLeft) // check if right edge is off screen
+            left = left + windowSize.width - tLeft - width;
+          if (height > scrollTop + windowSize.height - tTop) { // check is bottom edge goes off screen
+            if (scrollTop - tTop < scrollTop + windowSize.height - tTop - height) { // check if bottom edge is off screen
+              gravity = 's';
+              calcPos();
+            } else {
+              top  = scrollTop + windowSize.height - tTop + top - height;
+            }
+          }
+          break;
+
+        default: // case 's':
+          left   = pos[0] - (width / 2);
+          top    = pos[1] - height - dist;
+          tLeft  = left + offsetLeft(container);
+          tTop   = top + offsetTop(container);
+          if (tLeft < scrollLeft) // check if left edge is off screen
+            left = scrollLeft - tLeft + left;
+          if (width > windowSize.width - tLeft) // check if right edge is off screen
+            left = left + windowSize.width - tLeft - width;
+          if (scrollTop > tTop) { // check if top edge is off screen
+            if (scrollTop - tTop < scrollTop + windowSize.height - tTop - height) {
+              gravity = 'n';
+              calcPos();
+            } else {
+              top  = scrollTop;
+            }
+          }
+          break;
+      }
+    }
 
 
     // Account for scroll bars
